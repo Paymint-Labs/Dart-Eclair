f188a7cbfd24843b6d5322c88b035d034f039fee
Rohan Kapur <rohankapurdev@outlook.com>
2020-07-16 22:17:58 +0800

    ignore - checking response

--- android/src/main/java/com/paymintapp/dart_eclair/DartEclairPlugin.java
+++ android/src/main/java/com/paymintapp/dart_eclair/DartEclairPlugin.java
@@ -42,7 +42,7 @@ public class DartEclairPlugin implements FlutterPlugin, MethodCallHandler {
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + Build.VERSION_CODES.BASE);
+      result.success("Android " + Build.VERSION.RELEASE);
     } else {
       result.notImplemented();
     }
